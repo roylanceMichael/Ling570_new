13bb3c2fc8b4ba4248b5865ad36a5b693c2ac7a1
Mike Roylance <roylance.michael@gmail.com>
2013-11-18 20:17:15 -0700

    deleted files!

--- HW7/source/postuple.py
+++
@@ -1,5 +0,0 @@
-class POSTuple:
-	def __init__(self, pos, sym, prob):
-		self.pos = pos
-#		self.sym = sym
-		self.prob = prob
--- HW7/source/transition.py
+++
@@ -1,9 +0,0 @@
-class Transition:
-	def __init__(self, currentPos, previousPos, symbol, probability):
-		self.currentPos = currentPos
-		self.previousPos = previousPos
-		self.symbol = symbol
-		self.probability = probability
-
-	def printSelf(self):
-		return "toState: " + self.currentPos + " fromState: " + self.previousPos + " symbol: " + self.symbol + " prob: " + str(self.probability)
