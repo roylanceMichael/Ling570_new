b969c51e1c4b8905e46ba504f85d3992b8794e11
toOliya <toOliya@gmail.com>
2013-11-24 11:10:58 +0700

    q3 .sh

---
+++ HW8/create_vectors.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+python2.7 ./source/main_q3.py $@
--- HW8/source/main_q3.py
+++ HW8/source/main_q3.py
@@ -3,16 +3,14 @@ import vectors
 
 
 def main():
+
         vect = vectors.CreateVectors()
-        ratio = sys.argv[3]
-
-        dirs = sys.argv[4:]
-
-	trainFile = sys.argv[1]
-
-	testFile = sys.argv[2]
-	
-	result = vect.processDir(ratio, dirs, trainFile, testFile)
+	if len(sys.argv) > 4:
+		trainFile = sys.argv[1]
+		testFile = sys.argv[2]
+	        ratio = sys.argv[3]
+	        dirs = sys.argv[4:]
+		vect.processDir(ratio, dirs, trainFile, testFile)
 
 
 
